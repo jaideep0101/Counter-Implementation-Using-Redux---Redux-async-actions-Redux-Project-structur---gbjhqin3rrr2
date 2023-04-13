40740e9782cf7de3bda1a2dc604ee580be00a87f
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-04-13 14:04:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-
+const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter)
   return (
     <div id='main'>
-      
+       <h1 data-testid='counter'>{counter}</h1>
+     <button onClick={() =>  dispatch({ type: 'INCREMENT' })}>+</button>
+     <button onClick={() =>  dispatch({ type: 'DECREMENT' })}>-</button>
     </div>
   );
 }
